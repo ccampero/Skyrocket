cac8cfb719f33d2a0417851332c362265e821a89
mccampero <mccampero2@gmail.com>
2024-06-26 15:35:29 -0400

    add custom middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const applicationsController = require('./controllers/applications.js');
@@ -28,7 +30,19 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passUserToView); 
 
+app.get('/', (req, res) => {
+  if (req.session.user) {
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    res.render('index.ejs');
+  }
+});
+
+app.use('/auth', authController);
+app.use(isSignedIn); 
+app.use('/users/:userID/applications', applicationsController);
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
