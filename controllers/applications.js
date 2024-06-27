5ac2cca9eb58dd2c35f33d174204d9a8df78549c
mccampero <mccampero2@gmail.com>
2024-06-27 10:50:49 -0400

    add showpage for extrafunction

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,12 +3,19 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs')
-    } catch {
-        console.log(error)
-        res.redirect('/')
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Render index.ejs, passing in all of the current user's 
+      // applications as data in the context object. 
+      res.render('applications/index.ejs', {
+        applications: currentUser.applications,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error)
+      res.redirect('/')
     }
   });
 
@@ -33,6 +40,27 @@ router.post('/', async (req, res) => {
       res.redirect('/')
     }
   });
+
+router.get('/:applicationId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Find the application by the applicationId supplied from req.params
+      const application = currentUser.applications.id(req.params.applicationId);
+      // Render the show view, passing the application data in the context object
+      res.render('applications/show.ejs', {
+        application: application,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+  
+  
+
+  
   
 
 module.exports = router;
